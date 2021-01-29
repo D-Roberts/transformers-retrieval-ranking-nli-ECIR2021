e4e8c909e0ac2634316be582e2be2c2267aaa27a
Denisa Roberts <denisa.roberts@denisaroberts.me>
2021-01-29 17:42:13 -0500

    Change dir struct

--- download_scripts/download_fact_verification_model.sh
+++
@@ -1,8 +0,0 @@
-
-mkdir nli_model
-wget -O nli_model/enmbert_fine_tuned_rte.zip http://claimtraindata.s3.amazonaws.com/enmbert_fine_tuned_rte.zip
-unzip nli_model/enmbert_fine_tuned_rte.zip -d nli_model
-
-# copy model artifacts to out_dir_rte when ready to run api or make predictions
-# cp nli_model/model/* out_dir_rte
-# This is the fine-tuned EnmBERT
--- download_scripts/download_translated_data.sh
+++
@@ -1,6 +0,0 @@
-mkdir data/translated_data
-
-wget -O data/translated_data/translated_data.zip http://claimtraindata.s3.amazonaws.com/translated_data.zip
-unzip data/translated_data/translated_data.zip -d data/translated_data
-
-# use any of the file as input to nli predict script
