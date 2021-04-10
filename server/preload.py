38e2fbcd4aa10ce00f28cb1b4c86c9044f3b8214
Denisa Roberts <denisa.roberts@denisaroberts.me>
2021-04-10 12:23:11 -0400

    UUpdate prel

--- server/preload.py
+++ server/preload.py
@@ -9,7 +9,7 @@ from mfactcheck.pipelines.multi_nli import MultiNLIPipeline
 cur_dir = os.path.dirname(__file__)
 
 # simple caching of models before workers handle requests
-
-doc_retriever = Doc_Retrieval(add_claim=True, k_wiki_results=1)
-sentence_selector = MultiSentPipeline()
-verifier = MultiNLIPipeline()
+if __name__ == '__main__':
+    doc_retriever = Doc_Retrieval(add_claim=True, k_wiki_results=1)
+    sentence_selector = MultiSentPipeline()
+    verifier = MultiNLIPipeline()
